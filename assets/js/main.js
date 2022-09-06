f3436bd4dd3650b7378f9af6f24f65131cc3b9ce
Erik Sales <eriksales1@hotmail.com>
2022-09-06 16:10:36 -0400

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,89 @@
+const inputTarefa = document.querySelector('.input-nova-tarefa');
+const btnTarefa = document.querySelector('.bx-plus');
+const tarefas = document.querySelector('.tarefas');
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+inputTarefa.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+})
+
+function limpaInput() {
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaBotaoApagar(li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerHTML = "<i class='bx bx-trash'></i>";
+    //botaoApagar.innerText = 'delete';
+    //botaoApagar.setAttribute('class', 'apagar')
+    botaoApagar.classList.add('delete')
+    botaoApagar.setAttribute('title', 'apagar esta tarefa')
+    li.appendChild(botaoApagar);
+}
+
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li)
+    salvarTarefas();
+
+}
+
+
+btnTarefa.addEventListener('click', function () {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click', function (e) {
+    const el = e.target;
+    //const parentElement = el.closest("ul");
+
+    if (el.classList.contains('delete' || 'bx bx-trash')) {
+        el.parentElement.remove();
+        salvarTarefas();
+
+    }
+
+})
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listaDeTarefas = [];
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+        listaDeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+
+}
+
+function adicionaTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+
+    }
+
+    }
+
+
+adicionaTarefasSalvas();
